82de8d4f169fd3d4f6531026033a60c8cd79a105
AnesKostreba <aneskostreba@yahoo.com>
2023-11-25 16:19:30 +0100

    create entities and one service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { AdministratorService } from './services/administrator/administrator.service';
+import { Administrator } from 'entities/administrator.entity';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(
+    private administrator: AdministratorService
+  ) {}
 
   @Get()
   getHello(): string {
     return "Hello World!";
+  }
+
+  @Get('api/administrator')
+  getAllAdmin():Promise<Administrator[]>{
+    return this.administrator.getAll();
   }
 }
